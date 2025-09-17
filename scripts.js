e4ba38d00500c387fb61233c177c684c076bcd7d
franz sebastian <145816723+nanaobit@users.noreply.github.com>
2025-09-17 14:24:43 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,53 @@
-const params = {
-	appName:"graphing",
-	width:600,
-	heigth:400,
-	showToolBar:true,
-	showAlgebraInput:true,
-	showMenuBar:false
-}
-const applet = new GGBApplet(params, true)
-window.addEventListener("load",() => {
-	applet.inject("ggb-element")
-})
-
-function crearPunto(){
-	const x = (Math.random()*6-3).toFixed(2)
-	const y = (Math.random()*6-3).toFixed(2)
-	applet.setValue("A", '(${x} , ${y})')
-	applet.evalCommand('A = (${x}, ${y})')
-}
-function graficar(){
-	applet.evalCommand("f(x) = sin(x)")
-}
-
-
-
-
-
-
-
-
-
+const data = [{
+	x: [0,1,2,3,4],
+	y: [0,1,4,9,16],
+	type:'scatter',
+	mode: 'lines+markers'
+}];
+function saludar(){
+	console.log('hello world!!');
+};
+function graficar() {
+	const config = {
+		staticPlot: false
+	};
+	const x1 = parseFloat(document.getElementById("x1").value);
+	const y1 = parseFloat(document.getElementById("y1").value);
+	const x2 = parseFloat(document.getElementById("x2").value);
+	const y2 = parseFloat(document.getElementById("y2").value);
+	
+	const data2 = [{
+		x: [x1, y1],
+		y: [x2, y2],
+		type: 'scatter',
+		mode: 'lines+markers',
+		line: {
+			color: 'blue',
+			width: 3
+		},
+		marker: {
+			color:'red',
+			size: 10
+		},
+		fill:'tozeroy',
+		fillcolor: 'rgba(0 ,0 , 255, 0.2)'
+	}];
+	const layout = {
+		title: "ejemplo de grafico",
+		paper_bgcolor: "#1e1e2f",
+		plot_bgcolor: "#0f172a",
+		xaxis: {
+			title: 'eje x',
+			range: [0,10],
+			fixedrange: true
+		},
+		yaxis: {
+			title: 'eje y',
+			range: [0,20], 
+			fixedrange: true
+		}
+	};
+	Plotly.newPlot('aux', data2, layout,config);
+};
+graficar();
 
